57e610c5a1df0b2fdca2263ec3da162c620c3230
Reed O'Brien <reed@reedobrien.com>
2018-01-17 06:40:30 -0800

    Change comment style
    Make it easier to use the comments without reformatting.

--- bowling_test.go
+++ bowling_test.go
@@ -1,9 +1,7 @@
 package bowling_test
 
-/*
-1. All gutter balls -> 0
-2. All ones -> 20
-3. Spare + 3 + remaining 0 -> 16
-4. Strike + 3 + 4 + remaining 0 -> 24
-5. Perfect Game -> 300
-*/
+// 1. All gutter balls -> 0
+// 2. All ones -> 20
+// 3. Spare + 3 + remaining 0 -> 16
+// 4. Strike + 3 + 4 + remaining 0 -> 24
+// 5. Perfect Game -> 300
