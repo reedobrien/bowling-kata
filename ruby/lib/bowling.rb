89ffe020d5c55d9995a72db25b0197c9b5312ff0
Reed O'Brien <reed@reedobrien.com>
2022-05-22 11:52:41 -0700

    chore: remove ruby to get rid of dependabot alerts

--- ruby/lib/bowling.rb
+++
@@ -1,4 +0,0 @@
-require "bowling/version"
-
-module Bowling
-end
--- ruby/spec/bowling_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe Bowling do
-  # 1. All gutter balls -> 0
-  # 2. All ones -> 20
-  # 3. Spare + 3 + remaining 0 -> 16
-  # 4. Strike + 3 + 4 + remaining 0 -> 24
-  # 5. Perfect Game -> 300
-end
